7a93020dbee56a3bc96639b62bd003066f9fe9e2
GD-R <grgandharvar@gmail.com>
2021-09-30 22:44:16 +0530

    updated index file
    updated index file

--- Mark2/index.js
+++ Mark2/index.js
@@ -6,20 +6,20 @@ let high_score = [{
 {
   name: "RAVI",
   score: 3,
-}]
+}];
 
 let score = 0;
 let read_input = require("readline-sync");
 let user = read_input.question("Please Enter your name: ");
 
 // Array of 4 Questions
-let questions = ["What is NeoG Camp ? ", "Who is the Owner of NeoG Camp? ", "What Does NeoG Camp Offers? ", "How many levels are there in NeoG Camp? "]
+let questions = ["What is NeoG Camp ? ", "Who is the Owner of NeoG Camp? ", "What Does NeoG Camp Offers? ", "How many levels are there in NeoG Camp? ", "When is the last date for sumbission? "];
 
 // 2DArray of Set of Answers for each Question
-let answers = [["Bootcamp", "Online Course", "Other"], ["Ram", "Tanaypratap", "Pratap"], ["Data science", "WebDevlopment", "Digital Marketing"], ["Level 1", "Level 3", "Level 2"]];
+let answers = [["Bootcamp", "Online Course", "Other"], ["Ram", "Tanaypratap", "Pratap"], ["Data science", "WebDevlopment", "Digital Marketing"], ["Level 1", "Level 3", "Level 2"], ["Oct 30", "Sep 30", "Dec 30"]];
 
 // Array of Correct Answer
-let correct_ans = ["Bootcamp", "Tanaypratap", "WebDevlopment", "Level 2"];
+let correct_ans = ["Bootcamp", "Tanaypratap", "WebDevlopment", "Level 2", "Sep 30"];
 
 // Function to Display Question and Options
 function quiz (question, answer) {
